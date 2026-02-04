841c6a7acef7c12bd4569631ec9e0f47347c46c4
botagas <bitasid@gmail.com>
2026-02-05 01:17:36 +0200

    Apply bg changes

--- src/components/BreezyButton.js
+++ src/components/BreezyButton.js
@@ -2,10 +2,14 @@ import Button from '@enact/sandstone/Button';
 
 const joinClassNames = (...classNames) => classNames.filter(Boolean).join(' ');
 
-const BreezyButton = ({ className, ...rest }) => (
+const BreezyButton = ({ className, css, ...rest }) => (
 	<Button
 		{...rest}
 		className={joinClassNames('bf-button', className)}
+		css={{
+			...css,
+			bg: joinClassNames('bf-button-bg', css?.bg)
+		}}
 	/>
 );
 
